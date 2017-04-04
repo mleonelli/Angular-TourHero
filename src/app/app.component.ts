843ee9a799ae91d7a49146aca944b45c81df9cb2
Mauro Leonelli <mleonelli@gmail.com>
2017-04-04 15:12:30 +0200

    Modified AppComponent.ts to enable 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class Hero{
   template: `
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label> {{hero.id}}</div>
-    <div><label>name: </label> {{hero.name}}</div>`,
+    <div><label>id: </label> {{hero.id}}
+    <input [(ngModel)]="hero.name" placeholder="name"></div>`,
 })
 export class AppComponent  { 
   title = 'Tour of Heroes'; 

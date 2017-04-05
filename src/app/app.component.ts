fc550d5dcc69458f9a51efdef68806beee61cb96
Mauro Leonelli <mleonelli@gmail.com>
2017-04-05 14:17:19 +0200

    Added style to selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,19 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label> {{selectedHero.id}}
-      <input [(ngModel)]="selectedHero.name" placeholder="name"></div>
-    </div>
+    
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
-    ,</ul>`,
+    ,</ul>
+    
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label> {{selectedHero.id}}</div>
+      <div><label>name: </label><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
+    </div>`,
     styles: [`
   .selected {
     background-color: #CFD8DC !important;

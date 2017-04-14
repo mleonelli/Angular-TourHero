7f76717a29d4676805df4501fc5716ca71ead859
Mauro Leonelli <mleonelli@gmail.com>
2017-04-14 10:50:32 +0200

    Added the update functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'
 import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from "./hero-detail.component";
@@ -11,12 +12,15 @@ import { DashboardComponent } from "./dashboard.component";
 import { AppRoutingModule } from "./app-routing.module";
 
 
-
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   imports:      [ 
     BrowserModule, 
-    FormsModule, 
+    FormsModule,
+    HttpModule, 
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,10 +2,24 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Headers, Http } from "@angular/http";
+
+import 'rxjs/add/operator/toPromise';
+
 @Injectable()
-export class HeroService{
+export class HeroService {
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private heroesUrl = 'api/heroes';
+
+    constructor( private http: Http) {}
+
     getHeroes(): Promise<Hero[]> { 
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+        .toPromise()
+        .then(response => response.json().data as Hero[])
+        .catch(this.handleError);
+        //return Promise.resolve(HEROES);
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
@@ -16,6 +30,25 @@ export class HeroService{
     }
 
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes().then(heroes => heroes.find( hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+        //return this.getHeroes().then(heroes => heroes.find( hero => hero.id === id));
+    }
+
+    private handleError(error: any): Promise<any>{
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 }
